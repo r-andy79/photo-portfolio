ec9323184d69733b77202e2a4e583f47456a68e3
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-07-19 21:53:41 +0200

    Fetch modified to fetch two endpoints simultaneously

--- public/script.js
+++ public/script.js
@@ -16,17 +16,40 @@ formEl.addEventListener('submit', event => {
     body: JSON.stringify(body)
   }
   const user = loginUser('/', data);
-  user.then(response => response.json())
-    .catch(err => console.error(err))
-    .then(data => console.log(data))
-    .then(resp => getPhotos('/fotki'))
-    .then(resp => resp.json())
-    .then(data => {
-      data.forEach(item => {
-        photosContainer.innerHTML += `<div>filename: ${item.name}, author: ${item.author}</div>`
-      });
-    })
+  const allData = Promise.all([getUser('/user'), getPhotos('/fotki')])
+  
+  user.then(response => {
+    if(response.status !== 201) {
+      throw new Error('unable to log in')
+    }
+    return response.json()
+  })
+  .then(data => console.log(data))
+  .then(() => Promise.all([getUser('/user'), getPhotos('/fotki')]))
+  .then(res => console.log(res[0].firstName, res[1]))
+  .then(err => console.log(err))
 
+  // user.then(response => {
+  //   if(response.status !== 201) {
+  //     throw new Error('błąd');
+  //   }
+  //   return response.json()
+  // })
+  // .then(data => console.log(data))
+  // .then(() => getPhotos('/fotki'))
+  // .then(resp => resp.json())
+  // .then(data => {
+  //   data.forEach(item => {
+  //     photosContainer.innerHTML += `<div>filename: ${item.name}, author: ${item.author}</div>`
+  //   });
+  //   console.log('finished');
+  // })
+  // .catch(err => {
+  //   console.dir(err)
+  //   if(err.message === 'błąd') {
+  //     displayErrorMessage();
+  //   }
+  // })
 })
 
 function loginUser(url, body) {
@@ -34,5 +57,18 @@ function loginUser(url, body) {
 }
 
 function getPhotos(url) {
-  return fetch(url)
+  return fetch(url).then(res => res.json())
+}
+
+function getUser(url) {
+  return fetch(url).then(res => res.json())
+}
+
+// allData.then(res => console.log(res))
+
+
+function displayErrorMessage() {
+  // const p = document.createElement('p');
+  // p.textContent = 'Invalid credentials';
+  photosContainer.insertAdjacentHTML('beforebegin', '<p>Invalid credentials</p>')
 }
