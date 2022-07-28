977b6a9a12a2d018d4e08e39e711ae04c0851523
Andrzej Rakowski <andrzej.rakowski79@gmail.com>
2022-07-28 22:58:35 +0200

    added logout functionality, changed logic in admin view

--- public/script.js
+++ public/script.js
@@ -2,40 +2,90 @@ const formEl = document.querySelector('form');
 const photosContainer = document.querySelector('#photos');
 const nameEl = document.querySelector('#user');
 const err = document.querySelector('#error')
+const nav = document.querySelector('nav');
+const linksEl = Array.from(document.querySelectorAll('nav a'));
 
 
+function createForm() {
+  const form = document.createElement('form');
+  const labelLogin = document.createElement('label');
+  const inputEllogin = document.createElement('input');
+  const labelPassword = document.createElement('label');
+  const inputElpassword = document.createElement('input');
+  const submitEl = document.createElement('input');
 
-formEl.addEventListener('submit', event => {
-  event.preventDefault();
-  const userId = document.querySelector('[name="userId"]').value;
-  const password = document.querySelector('[name="password"]').value;
-  const body = { userId, password };
-  const data = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(body)
+  labelLogin.textContent = 'Username:'
+  labelPassword.textContent = 'Password:'
+  inputEllogin.setAttribute('name', 'userId');
+  inputEllogin.setAttribute('type', 'text');
+  inputElpassword.setAttribute('type', 'password');
+  inputElpassword.setAttribute('name', 'password');
+  inputElpassword.setAttribute('autocomplete', 'true')
+  submitEl.setAttribute('type', 'submit')
+  submitEl.value = 'Login';
+
+  labelLogin.appendChild(inputEllogin);
+  labelPassword.appendChild(inputElpassword);
+  form.appendChild(labelLogin);
+  form.appendChild(labelPassword)
+  form.appendChild(submitEl)
+  document.body.insertBefore(form, photosContainer);
+
+  form.addEventListener('submit', event => {
+    event.preventDefault();
+    console.log('handler')
+    const userId = document.querySelector('[name="userId"]').value;
+    const password = document.querySelector('[name="password"]').value;
+    const body = { userId, password };
+    const data = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(body)
+    }
+  
+    loginUser(data).then(response => {
+      cleanView()
+      if (response.status !== 201) {
+        throw new Error('unable to log in')
+      }
+    })
+    .then(() => {
+      addLogout()
+      getUser().then(displayUser)
+      getPhotos().then(displayPhotos)
+    })
+    .catch(err => {
+      if (err.message === 'unable to log in') {
+        displayErrorMessage(err.message)
+      }
+    })
+  })
+};
+
+function addLogout() {
+  const anchor = document.createElement('a');
+  anchor.textContent = 'logout';
+  console.log(anchor);
+  nav.appendChild(anchor)
+  
+  anchor.addEventListener('click', e => {
+    fetch('/logout').then((data) => {
+      displayErrorMessage(data.message)
+    })
+  })
+}
+
+
+function deleteFormIfExists() {
+  const form = document.querySelector('form');
+  if(form) {
+    document.body.removeChild(form);
   }
- 
-  loginUser(data).then(response => {
-    cleanView()
-    if (response.status !== 201) {
-      throw new Error('unable to log in')
-    }
-  })
-  .then(() => {
-    getUser().then(displayUser)
-    getPhotos().then(displayPhotos)
-  })
-  .catch(err => {
-    if (err.message === 'unable to log in') {
-      displayErrorMessage()
-    }
-  })
-})
+}
 
-function displayPhotos(photos){
+function displayPhotos(photos) {
   photos.forEach(photo => {
     photosContainer.innerHTML += `<div>filename: ${photo.name}, author: ${photo.author}</div>`
   })
@@ -43,8 +93,8 @@ function displayPhotos(photos){
 function displayUser(user) {
   nameEl.innerHTML = `Hello ${user.firstName}`;
 }
-function displayErrorMessage() {
-  err.innerHTML = 'Invalid credentials';
+function displayErrorMessage(message) {
+  err.innerHTML = message;
 }
 
 function loginUser(body) {
@@ -56,7 +106,13 @@ function getPhotos() {
 }
 
 function getUser() {
-  return fetch('/user').then(res => res.json())
+  return fetch('/user').then(res => {
+    if(res.status === 401) {
+      throw new Error('unauthorized', {cause: {status: res.status}})
+    } else {
+      return res.json()
+    }
+  })
 }
 
 function cleanView() {
@@ -64,3 +120,58 @@ function cleanView() {
   photosContainer.innerHTML = '';
   nameEl.innerHTML = '';
 }
+
+function homeView() {
+  cleanView();
+  console.log('home view');
+  getPhotos().then(photos => displayPhotos(photos))
+}
+
+function loginView() {
+  cleanView();
+  console.log('login view');
+  createForm()
+}
+
+function adminView() {
+  cleanView();
+  
+  return getUser().then(user => {
+    displayUser(user)
+    getPhotos().then(photos => displayPhotos(photos))
+  })
+}
+
+window.addEventListener('hashchange', e => {
+  console.log('hashchange');
+})
+
+window.addEventListener('popstate', e => {
+  console.log(e);
+})
+
+const onNavItemClick = path => {
+  window.history.pushState({}, "", path)
+}
+
+linksEl.forEach(el => {
+  el.addEventListener('click', e => {
+    onNavItemClick(e.target.hash);
+    if(e.target.hash === "#/login") {
+      deleteFormIfExists()
+      loginView()
+    } else if(e.target.hash === "#/") {
+      deleteFormIfExists()
+      homeView()
+    } else if(e.target.hash === "#/admin") {
+      deleteFormIfExists()
+      adminView().catch(() => {
+        window.history.pushState({}, "", '#/login');
+        loginView()
+      })
+    } else if(e.target.hash === "#/contact") {
+      deleteFormIfExists()
+      contactView()
+    }
+  })
+})
